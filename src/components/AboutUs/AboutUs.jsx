dab583e53b9233bbaa72f68eb6f06dbb46ec6308
kaullean <leandrokaul@hotmail.com>
2025-02-17 23:20:56 -0300

    Se continuo refactorizando AboutUs y atomizando componentes

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -2,24 +2,23 @@ import React from 'react';
 import './AboutUs.css';
 import ImageCard from '../ImageCard/ImageCard';
 import ArticleCard from '../ArticleCard/ArticleCard';
+import Banner from '../Banner/Banner'
+
 function AboutUs() {
     let banner = 'https://images.pexels.com/photos/17626465/pexels-photo-17626465/free-photo-of-comida-acera-frutas-tienda.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
     let rightImg='https://images.pexels.com/photos/2255935/pexels-photo-2255935.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
     let leftImg ='https://images.pexels.com/photos/262967/pexels-photo-262967.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
     let leftText=`Somos expertos en frescura y calidad, con más de 40 años de experiencia seleccionando los mejores productos para nuestros clientes. Nuestro compromiso es ofrecerte productos frescos, de alta calidad y a precios competitivos.`
-    let rightText=`diariamente recorremos cada rincón del Mercado Central, seleccionando con cuidado las frutas y verduras de productores nacionales e internacionales para brindarte una gran variedad con la mejor calidad, al mejor precio.`
+    let rightText=`Diariamente recorremos cada rincón del Mercado Central, seleccionando con cuidado las frutas y verduras de productores nacionales e internacionales para brindarte una gran variedad con la mejor calidad, al mejor precio.`
         
     return (
         <section className="aboutUs">
           {/* Banner */}
-          <figure className="aboutUs-banner">
-            <img src={banner} alt="Banner de la empresa" />
-          </figure>
-    
+          
+          <Banner src={banner} alt="Banner de la empresa"/>
           {/* Texto Izquierdo */}
-          <article className="aboutUs-article">
-            <p>{leftText}</p>
-          </article>
+          <ArticleCard text={leftText}/>
+
     
           {/* Imagen Derecha */}
           <ImageCard src={rightImg} alt="Banner de la empresa" />
@@ -30,10 +29,8 @@ function AboutUs() {
 
     
           {/* Texto Derecho */}
-          <article className="aboutUs-article">
-            <p>{rightText}</p>
-          </article>
-        </section>
+          <ArticleCard text={rightText}/>
+          </section>
       );
 }
 
---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Banner.css';
+function Banner({ src,alt }) {
+  return (
+    <figure className="aboutUs-banner">
+      <img src={src} alt={alt} />
+    </figure>
+  );
+}
+
+export default Banner;
